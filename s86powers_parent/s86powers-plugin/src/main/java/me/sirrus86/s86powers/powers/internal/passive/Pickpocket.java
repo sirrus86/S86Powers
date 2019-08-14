f00b4595dc3167255bf0425061db5002e20faaf5
BuildTools <unconfigured@null.spigotmc.org>
2019-08-14 17:07:23 -0400

    Alert victim when theft fails

--- s86powers_parent/s86powers-plugin/src/main/java/me/sirrus86/s86powers/powers/internal/passive/Pickpocket.java
+++ s86powers_parent/s86powers-plugin/src/main/java/me/sirrus86/s86powers/powers/internal/passive/Pickpocket.java
@@ -2,10 +2,10 @@ package me.sirrus86.s86powers.powers.internal.passive;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.bukkit.ChatColor;
+import org.bukkit.EntityEffect;
 import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.LivingEntity;
@@ -24,8 +24,6 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.loot.LootContext;
 import org.bukkit.loot.Lootable;
 
-import com.google.common.collect.Lists;
-
 import me.sirrus86.s86powers.powers.Power;
 import me.sirrus86.s86powers.powers.PowerManifest;
 import me.sirrus86.s86powers.powers.PowerStat;
@@ -34,14 +32,14 @@ import me.sirrus86.s86powers.tools.PowerTools;
 import me.sirrus86.s86powers.users.PowerUser;
 import me.sirrus86.s86powers.utils.PowerTime;
 
-@PowerManifest(name = "Pickpocket", type = PowerType.PASSIVE, author = "sirrus86", concept = "grimm294", icon=Material.CHEST,
+@PowerManifest(name = "Pickpocket", type = PowerType.PASSIVE, author = "sirrus86", concept = "grimm294", icon = Material.CHEST,
 	description = "Sneaking makes you partially invisible. Right-clicking any monster or player while barehanded and sneaking will make you steal a random item from their inventory. Taking items from the target's inventory has a [brChance]% chance to break your stealth. Taking damage will also break stealth. Stealing has a [cooldown] cooldown.")
-public class Pickpocket extends Power {
+public final class Pickpocket extends Power {
 
 	private Set<LivingEntity> noDrop;
 	private Set<PowerUser> stealth;
 	private double brChance;
-	private boolean dontDrop, goToInv, repeat;
+	private boolean dontDrop, goToInv, lookAtThief, repeat;
 	private PowerStat thefts;
 	
 	@Override
@@ -53,9 +51,7 @@ public class Pickpocket extends Power {
 	@Override
 	protected void onDisable(PowerUser user) {
 		if (stealth.contains(user)) {
-			PowerTools.poof(user.getPlayer().getLocation());
-			PowerTools.removeGhost(user.getPlayer());
-			stealth.remove(user);
+			removeStealth(user);
 		}
 	}
 
@@ -65,17 +61,32 @@ public class Pickpocket extends Power {
 		cooldown = option("steal-cooldown", PowerTime.toMillis(5, 0), "Amount of time after stealing before you can pickpocket again.");
 		dontDrop = option("no-drops-after-theft", true, "Whether non-player pickpocket victims should no longer be able to drop items on death.");
 		goToInv = option("steal-to-inventory", true, "Whether items stolen should go directly to inventory.");
+		lookAtThief = option("look-at-thief", true, "Whether players should look at thieves when they are detected.");
 		repeat = option("steal-repeatedly", true, "Whether user can attempt to steal from same non-player target repeatedly.");
 		thefts = stat("items-stolen", 50, "Items stolen", "Stealing items can no longer break stealth.");
 	}
 	
+	private void removeStealth(PowerUser user) {
+		user.getPlayer().playEffect(EntityEffect.ENTITY_POOF);
+		PowerTools.removeGhost(user.getPlayer());
+		stealth.remove(user);
+	}
+	
 	private ItemStack getDrop(LivingEntity entity, HumanEntity killer) {
 		if (entity instanceof HumanEntity) {
+			ItemStack drop = null;
 			PlayerInventory inv = ((HumanEntity) entity).getInventory();
-			List<ItemStack> items = Lists.newArrayList(inv.getContents());
-			int id = random.nextInt(items.size()) - 1;
-			ItemStack drop = new ItemStack(items.get(id).getType(), 1);
-			inv.removeItem(drop);
+			for (int i = 0; i < inv.getContents().length; i ++) {
+				int j = random.nextInt(inv.getContents().length);
+				if (inv.getContents()[j] != null) {
+					drop = inv.getContents()[j].clone();
+					inv.getContents()[j].setAmount(inv.getContents()[j].getAmount() - 1);
+					break;
+				}
+			}
+			if (drop != null) {
+				drop.setAmount(1);
+			}
 			return drop;
 		}
 		else if (entity instanceof Lootable) {
@@ -96,9 +107,7 @@ public class Pickpocket extends Power {
 			if (user.allowPower(this)
 					&& stealth.contains(user)) {
 				user.sendMessage(ChatColor.RED + "Your stealth was broken.");
-				PowerTools.poof(user.getPlayer().getLocation());
-				PowerTools.removeGhost(user.getPlayer());
-				stealth.remove(user);
+				removeStealth(user);
 			}
 		}
 	}
@@ -118,9 +127,7 @@ public class Pickpocket extends Power {
 			if (user.allowPower(this)
 					&& stealth.contains(user)) {
 				user.sendMessage(ChatColor.RED + "You've been detected.");
-				PowerTools.poof(user.getPlayer().getLocation());
-				PowerTools.removeGhost(user.getPlayer());
-				stealth.remove(user);
+				removeStealth(user);
 			}
 		}
 	}
@@ -148,13 +155,20 @@ public class Pickpocket extends Power {
 						if (random.nextDouble() < (brChance / 100.0D)
 								&& !user.hasStatMaxed(thefts)) {
 							if (target instanceof Monster) {
-								if (target instanceof PigZombie) ((PigZombie) target).setAngry(true);
+								if (target instanceof PigZombie) {
+									((PigZombie) target).setAngry(true);
+								}
 								((Monster) target).setTarget(user.getPlayer());
 							}
+							else if (target instanceof Player) {
+								PowerUser victim = getUser((Player) target);
+								victim.sendMessage(ChatColor.RED + user.getName() + ChatColor.RED + " tried to steal from you!");
+								if (lookAtThief) {
+									PowerTools.setLook((Player) target, user.getPlayer().getEyeLocation());
+								}
+							}
 							user.sendMessage(ChatColor.RED + "You were caught pickpocketing.");
-							PowerTools.poof(user.getPlayer().getLocation());
-							PowerTools.removeGhost(user.getPlayer());
-							stealth.remove(user);
+							removeStealth(user);
 						}
 						if (!(target instanceof Player)
 								&& !repeat) {
@@ -162,6 +176,9 @@ public class Pickpocket extends Power {
 						}
 						user.increaseStat(thefts, 1);
 						user.setCooldown(this, cooldown);
+					}
+					else {
+						user.sendMessage(ChatColor.RED + "Failed to steal anything.");
 					}
 				}
 			}
@@ -173,14 +190,12 @@ public class Pickpocket extends Power {
 		PowerUser user = getUser(event.getPlayer());
 		if (user.allowPower(this)) {
 			if (event.isSneaking()) {
-				PowerTools.poof(user.getPlayer().getLocation());
+				user.getPlayer().playEffect(EntityEffect.ENTITY_POOF);
 				PowerTools.addGhost(user.getPlayer());
 				stealth.add(user);
 			}
 			else {
-				PowerTools.poof(user.getPlayer().getLocation());
-				PowerTools.removeGhost(user.getPlayer());
-				stealth.remove(user);
+				removeStealth(user);
 			}
 		}
 	}
