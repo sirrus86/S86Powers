c01719957a6f392a09ebb3fad988a91482179bd8
sirrus86 <sirrus86@hotmail.com>
2019-08-19 13:19:53 -0400

    Make bats fly, prevent interaction while transformed

--- s86powers_parent/s86powers-plugin/src/main/java/me/sirrus86/s86powers/powers/internal/passive/Vampirism.java
+++ s86powers_parent/s86powers-plugin/src/main/java/me/sirrus86/s86powers/powers/internal/passive/Vampirism.java
@@ -1,7 +1,9 @@
 package me.sirrus86.s86powers.powers.internal.passive;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.ChatColor;
@@ -17,22 +19,23 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import me.sirrus86.s86powers.events.PowerUseEvent;
+import me.sirrus86.s86powers.events.UserMaxedStatEvent;
 import me.sirrus86.s86powers.powers.Power;
 import me.sirrus86.s86powers.powers.PowerManifest;
 import me.sirrus86.s86powers.powers.PowerStat;
 import me.sirrus86.s86powers.powers.PowerType;
 import me.sirrus86.s86powers.tools.PowerTools;
+import me.sirrus86.s86powers.tools.version.MCVersion;
 import me.sirrus86.s86powers.users.PowerUser;
-import me.sirrus86.s86powers.users.UserContainer;
-import me.sirrus86.s86powers.utils.PowerTime;
 
 @PowerManifest(name = "Vampirism", type = PowerType.PASSIVE, author = "sirrus86", concept = "TheClownOfCrime", icon=Material.FERMENTED_SPIDER_EYE,
 	description = "You rapidly lose hunger while in direct sunlight, eventually igniting you[helmProt] unless you are wearing a helmet[/helmProt]. While sprinting, running speed and jump height increase. Immune to fall damage. Wooden tools do [wMult]x more damage to you.")
@@ -40,20 +43,32 @@ public final class Vampirism extends Power {
 
 	private Set<PowerUser> sprinting, transformed;
 	
+	private final Map<Integer, Object> flyingMeta = new HashMap<>();
+	
 	private boolean helmProt, infect;
 	private int food, jmp, spd;
 	private double infectChance, wMult;
 	private PowerStat kills;
+	private String infected, turnToBat, turnToHuman;
 	
 	@Override
 	protected void onEnable() {
+		flyingMeta.put(MCVersion.isLessThan(MCVersion.v1_14) ? 12 : 14, (byte) 0x00);
 		sprinting = new HashSet<>();
 		transformed = new HashSet<>();
 		runTaskTimer(manage, 0L, 5L);
 	}
+	
+	@Override
+	protected void onEnable(PowerUser user) {
+		if (user.hasStatMaxed(kills)) {
+			user.setAllowFlight(true);
+		}
+	}
 
 	@Override
 	protected void onDisable(PowerUser user) {
+		user.setAllowFlight(false);
 		if (sprinting.contains(user)) {
 			user.removePotionEffect(PotionEffectType.JUMP);
 			user.removePotionEffect(PotionEffectType.SPEED);
@@ -66,16 +81,17 @@ public final class Vampirism extends Power {
 
 	@Override
 	protected void options() {
-		cooldown = option("superpower.cooldown", PowerTime.toMillis(10, 0), "Amount of time before user can transform again.");
 		food = option("food-regen", 7, "Amount of food regeneration granted by killing other entities.");
 		helmProt = option("helmet-prevents-ignition", false, "Whether wearing a helmet will prevent vampires from igniting in sunlight.");
 		infect = option("infect-other-players", false, "Whether attacks from vampires should infect players without the power.");
 		infectChance = option("infect-chance", 15.0D, "Percent chance that other players will be infected.");
-		item = option("superpower.item", new ItemStack(Material.FERMENTED_SPIDER_EYE, 1), "Item used to transform from/to a bat.");
 		jmp = option("jump-degree", 1, "Jump effect to apply to user while sprinting.");
-		kills = stat("vampire-kills", 100, "Kills as a vampire", "[act:item]ing while holding [item] transforms you into a bat, allowing you to fly. Exposure to sunlight will end the transformation.");
+		kills = stat("vampire-kills", 100, "Kills as a vampire", "Able to transform into a bat and fly. Exposure to sunlight will end the transformation.");
 		spd = option("speed-degree", 3, "Speed effect to apply to user while sprinting.");
 		wMult = option("wood-damage-multiplier", 3.0D, "Amount to multiply damage by when caused by wooden items.");
+		infected = locale("message.been-infected", ChatColor.RED + "You've been infected with [power]!");
+		turnToHuman = locale("message.turn-to-human", ChatColor.YELLOW + "You return to human form.");
+		turnToBat = locale("message.turn-to-bat", ChatColor.GREEN + "You transform into a bat.");
 	}
 	
 	private BukkitRunnable manage = new BukkitRunnable() {
@@ -86,7 +102,7 @@ public final class Vampirism extends Power {
 				PowerUser user = it.next();
 				if (user.isOnline()
 						&& user.allowPower(getInstance())) {
-					if (PowerTools.inSunlight(user.getPlayer().getLocation())) {
+					if (PowerTools.inSunlight(user.getPlayer().getEyeLocation())) {
 						if (transformed.contains(user)) {
 							transform(user, false);
 						}
@@ -100,6 +116,10 @@ public final class Vampirism extends Power {
 							user.getPlayer().setFireTicks(20);
 						}
 					}
+					else if (user.hasStatMaxed(kills)
+							&& user.getCooldown(getInstance()) < 0L) {
+						user.setAllowFlight(true);
+					}
 				}
 			}
 		}
@@ -107,16 +127,25 @@ public final class Vampirism extends Power {
 	
 	private void transform(PowerUser user, boolean transform) {
 		user.getPlayer().playEffect(EntityEffect.ENTITY_POOF);
-		user.getPlayer().setAllowFlight(transform);
 		if (transform) {
-			PowerTools.addDisguise(user.getPlayer(), EntityType.BAT);
+			PowerTools.addDisguise(user.getPlayer(), EntityType.BAT, flyingMeta);
 			transformed.add(user);
-			user.sendMessage(ChatColor.GREEN + "You transform into a bat.");
+			user.sendMessage(turnToBat);
 		}
 		else {
 			PowerTools.removeDisguise(user.getPlayer());
 			transformed.remove(user);
-			user.sendMessage(ChatColor.GREEN + "You return to human form.");
+			user.getPlayer().setFlying(false);
+			user.sendMessage(turnToHuman);
+		}
+	}
+	
+	@EventHandler
+	private void noInteract(PlayerInteractEvent event) {
+		PowerUser user = getUser(event.getPlayer());
+		if (user.allowPower(this)
+				&& transformed.contains(user)) {
+			event.setCancelled(true);
 		}
 	}
 	
@@ -133,7 +162,14 @@ public final class Vampirism extends Power {
 	
 	@EventHandler (ignoreCancelled = true)
 	private void onDmg(EntityDamageByEntityEvent event) {
-		if (event.getEntity() instanceof Player
+		if (event.getDamager() instanceof Player) {
+			PowerUser user = getUser((Player) event.getDamager());
+			if (transformed.contains(user)) {
+				event.setCancelled(true);
+			}
+		}
+		if (!event.isCancelled()
+				&& event.getEntity() instanceof Player
 				&& event.getDamager() instanceof LivingEntity) {
 			PowerUser user = getUser((Player) event.getEntity());
 			LivingEntity attacker = (LivingEntity) event.getDamager();
@@ -150,13 +186,12 @@ public final class Vampirism extends Power {
 			else if (attacker instanceof Player
 					&& infect) {
 				PowerUser pAttacker = getUser((Player) attacker);
-				UserContainer vCont = UserContainer.getContainer(user);
 				if (pAttacker.allowPower(this)
-						&& !vCont.hasPower(this)
-						&& !vCont.hasPower("Lycanthropy")
+						&& !user.hasPower(this)
+						&& !user.hasPower("Lycanthropy")
 						&& random.nextDouble() < (infectChance / 100.0D)) {
-					user.sendMessage(ChatColor.RED + "You've been infected with Vampirism!");
-					vCont.addPower(this, true);
+					user.sendMessage(infected.replace("[power]", this.getName()));
+					user.addPower(this, true);
 				}
 			}
 		}
@@ -191,22 +226,11 @@ public final class Vampirism extends Power {
 	}
 	
 	@EventHandler(ignoreCancelled = true)
-	private void onUse(PowerUseEvent event) {
-		if (event.getPower() == this) {
-			PowerUser user = event.getUser();
-			if (user.hasStatMaxed(kills)) {
-				if (transformed.contains(user)) {
-					transform(user, false);
-				}
-				else {
-					if (user.getCooldown(this) <= 0) {
-						transform(user, true);
-					}
-					else {
-						user.showCooldown(this);
-					}
-				}
-			}
+	private void onFly(PlayerToggleFlightEvent event) {
+		PowerUser user = getUser(event.getPlayer());
+		if (user.allowPower(this)
+				&& user.hasStatMaxed(kills)) {
+			transform(user, !transformed.contains(user));
 		}
 	}
 	
@@ -224,6 +248,14 @@ public final class Vampirism extends Power {
 				user.removePotionEffect(PotionEffectType.JUMP);
 				user.removePotionEffect(PotionEffectType.SPEED);
 			}
+		}
+	}
+	
+	@EventHandler
+	protected void onStatMax(UserMaxedStatEvent event) {
+		PowerUser user = event.getUser();
+		if (event.getStat() == kills) {
+			user.setAllowFlight(true);
 		}
 	}
 
